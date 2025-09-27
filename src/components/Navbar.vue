a98cb892fb9de1e4fb31f96c385ab2cfcea9326e
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-09-27 15:55:06 -0400

    feat: updated navbar with auth views

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,17 @@
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/authStore.js';
+import Button from '@/components/Button.vue';
+
+const authStore = useAuthStore();
+</script>
 
 <template>
     <nav>
         <router-link to="/">Play</router-link>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/register">Register</router-link>
+        <router-link v-if="!authStore.isAuthenticated" to="/login">Login</router-link>
+        <router-link v-if="!authStore.isAuthenticated" to="/register">Register</router-link>
+        <router-link v-if="authStore.isAuthenticated" to="/profile">Profile</router-link>
+        <Button v-if="authStore.isAuthenticated" @click="authStore.logout" text="Logout" />
     </nav>
 </template>
 
