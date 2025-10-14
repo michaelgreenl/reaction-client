886c38c6b83f557abdf2c06b5b41cd3e1029f5c7
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-14 14:56:13 -0400

    feat: added initial styles/preset for button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,11 +4,42 @@ defineEmits(['click']);
 const props = defineProps({
     text: { type: String },
     showText: { type: Boolean, default: true },
+    preset: { type: String, default: 'primary' },
 });
 </script>
 
 <template>
-    <button @click="$emit('click')">
-        {{ showText ? text : null }}
+    <button :class="preset" @click="$emit('click')">
+        <span>{{ showText ? text : null }}</span>
     </button>
 </template>
+
+<style lang="scss" scoped>
+button {
+    position: relative;
+    cursor: pointer;
+    font-family: $primary-font-stack;
+    border: 0;
+    padding: 0.25em 1em;
+    border-radius: 8px;
+
+    span {
+        font-size: 1.1em;
+        white-space: nowrap;
+        letter-spacing: 0.05em;
+    }
+
+    &.primary {
+        background: $color-gray2;
+    }
+
+    &.secondary {
+        background: $color-accent;
+    }
+
+    &:disabled {
+        background: $color-gray5;
+        color: $color-gray3;
+    }
+}
+</style>
