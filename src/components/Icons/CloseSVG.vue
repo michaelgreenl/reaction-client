51ebcf8e5eed67b4334ea6ace8c1cb8c7eb796a8
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-17 15:39:53 -0400

    feat: added 'X' icon

---
+++ src/components/Icons/CloseSVG.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M67.5 7.5L7.59623 67.4982M7.5 7.5L67.402 67.5" stroke-linecap="round" stroke-width="12" />
+    </svg>
+</template>
