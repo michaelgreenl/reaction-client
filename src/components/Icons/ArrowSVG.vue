c4f7e70af258cbb65f16bb70ca87a55185787934
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-19 17:14:38 -0400

    refactor: changed stroke width

--- src/components/Icons/ArrowSVG.vue
+++ src/components/Icons/ArrowSVG.vue
@@ -4,7 +4,7 @@
             d="M67.5 22.5L37.5 52.5M7.5 22.5L37.5481 52.5"
             transform-origin="37.5px 37.5px"
             style="transform: rotate(-90deg); transform-origin: 37.5px 37.5px"
-            stroke-width="14"
+            stroke-width="16"
             stroke-linecap="round"
         ></path>
     </svg>
