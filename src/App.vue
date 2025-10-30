33f4e137f3b266c9138f8826df3c2c98ed875432
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-10-30 15:56:35 -0400

    feat: improved responsiveness with smaller font-size on xsm-phone in #app

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,11 @@ body,
     position: relative;
     display: flex;
     flex-direction: column;
+    font-size: 0.9em;
+
+    @include bp-xsm-phone {
+        font-size: 1em;
+    }
 
     @include bp-xxl-desktop {
         font-size: 1.2em;
