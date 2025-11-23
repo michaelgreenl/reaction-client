e1b11eb03fd063629268c869eddf37ea59d7a68b
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-11-23 17:10:11 -0500

    feat: added GameHistoryTable component for the ProfileView table

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -8,6 +8,7 @@ import { formatDate, formatTime } from '@/util/time.js';
 import { gsap } from 'gsap';
 import Flip from 'gsap/Flip';
 import Button from '@/components/Button.vue';
+import GameHistoryTable from '@/components/GameHistoryTable.vue';
 import Loader from '@/components/Loader.vue';
 import RangeInput from '@/components/Inputs/Range.vue';
 import NumberInput from '@/components/Inputs/Number.vue';
@@ -429,114 +430,23 @@ function toggleFilterDropdown() {
                         </div>
                     </form>
                 </div>
-                <div class="table-wrapper">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>
-                                    <Button
-                                        preset="primary-alt"
-                                        :text="`${activeGames.sorted.by !== 'score' ? 'Score' : activeGames.sorted.order === 'ASC' ? '▴ Score' : '▾ Score'}`"
-                                        :disabled="loadingGames"
-                                        @click="handleSort('score')"
-                                    />
-                                </th>
-                                <th>
-                                    <Button
-                                        preset="primary-alt"
-                                        :text="`${activeGames.sorted.by !== 'time' ? 'Time' : activeGames.sorted.order === 'ASC' ? '▴ Time' : '▾ Time'}`"
-                                        :disabled="loadingGames"
-                                        @click="handleSort('time')"
-                                    />
-                                </th>
-                                <th v-if="showSettings">
-                                    <Button text="Circle Size" class="setting" />
-                                </th>
-                                <th v-if="showSettings">
-                                    <Button text="Spawn Interval" class="setting" />
-                                </th>
-                                <th v-if="showSettings">
-                                    <Button text="Shrink Time" class="setting" />
-                                </th>
-                                <th>
-                                    <Button
-                                        preset="primary-alt"
-                                        :text="`${activeGames.sorted.by !== 'createdAt' ? 'Date' : activeGames.sorted.order === 'ASC' ? '▴ Date' : '▾ Date'}`"
-                                        :disabled="loadingGames"
-                                        @click="handleSort('createdAt')"
-                                    />
-                                </th>
-                            </tr>
-                        </thead>
-                        <div v-if="loadingGames" class="loader">
-                            <Loader text="Loading" />
-                        </div>
-                        <div v-if="activeGames.games.length === 0 && !loadingGames" class="loader">
-                            <span>No Games Found.</span>
-                        </div>
-                        <tbody
-                            :style="{
-                                // hiding element so the height can still affect the table size
-                                visibility: `${loadingGames ? 'hidden' : 'visible'}`,
-                                height: `${activeGames.games.length > 0 ? activeGames.games.length * 26 : 260}px`,
-                            }"
-                        >
-                            <tr v-for="game in activeGames.games" :key="game.createdAt">
-                                <td :class="`${activeGames.sorted.by === 'score' ? 'sorted' : undefined}`">
-                                    {{ game.score }}
-                                </td>
-                                <td :class="`${activeGames.sorted.by === 'time' ? 'sorted' : undefined}`">
-                                    {{ formatTime(game.time) }}
-                                </td>
-                                <td v-if="showSettings">{{ game.settings.circleSize }}px</td>
-                                <td v-if="showSettings">
-                                    {{
-                                        game.settings.spawnInterval.toString().length === 4
-                                            ? game.settings.spawnInterval.toFixed(2)
-                                            : game.settings.spawnInterval.toFixed(1)
-                                    }}
-                                </td>
-                                <td v-if="showSettings">
-                                    {{
-                                        game.settings.shrinkTime.toString().length === 4
-                                            ? game.settings.shrinkTime.toFixed(2)
-                                            : game.settings.shrinkTime.toFixed(1)
-                                    }}
-                                </td>
-                                <td
-                                    class="date"
-                                    :class="`${activeGames.sorted.by === 'createdAt' ? 'sorted' : undefined}`"
-                                >
-                                    <span>
-                                        {{ formatDate(game.createdAt) }}
-                                    </span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="table-nav">
-                    <Button
-                        preset="primary-alt"
-                        text="prev"
-                        :disabled="loadingGames || offset === 0"
-                        @click="switchPage((offset -= 10), activePage - 1)"
-                    />
-                    <span>
-                        {{ activePage }}
-                    </span>
-                    <Button
-                        preset="primary-alt"
-                        text="next"
-                        :disabled="
-                            loadingGames ||
-                            authStore.userStats?.totalGames < 10 ||
-                            activeGames.games.length === 0 ||
-                            offset + 10 >= authStore.userStats?.totalGames
-                        "
-                        @click="switchPage((offset += 10), activePage + 1)"
-                    />
-                </div>
+                <GameHistoryTable
+                    :games="activeGames.games"
+                    :loading="loadingGames"
+                    :show-settings="showSettings"
+                    :sort-by="activeGames.sorted.by"
+                    :sort-order="activeGames.sorted.order"
+                    :page="activePage"
+                    :disable-prev="offset === 0"
+                    :disable-next="
+                        authStore.userStats?.totalGames < 10 ||
+                        activeGames.games.length === 0 ||
+                        offset + 10 >= authStore.userStats?.totalGames
+                    "
+                    @sort="handleSort"
+                    @prev-page="switchPage((offset -= 10), activePage - 1)"
+                    @next-page="switchPage((offset += 10), activePage + 1)"
+                />
             </div>
         </div>
     </div>
@@ -876,139 +786,6 @@ function toggleFilterDropdown() {
                 opacity: 0;
             }
         }
-    }
-}
-
-.table-wrapper {
-    position: relative;
-    z-index: 2;
-    padding: $size-1 $size-2;
-    max-width: 17em;
-    overflow-x: scroll;
-
-    @include bp-custom-min(450) {
-        max-width: 20em;
-    }
-
-    @include bp-sm-phone {
-        overflow-x: hidden;
-        max-width: 34em;
-    }
-
-    &::-webkit-scrollbar {
-        width: 6px !important;
-        height: 6px !important;
-        background: $color-bg-secondary;
-    }
-
-    &::-webkit-scrollbar-thumb {
-        background: $color-gray3;
-        border-radius: 6px;
-
-        &:hover {
-            background: darken-color($color-gray3, 5%);
-        }
-
-        &:active {
-            background: darken-color($color-gray3, 10%);
-        }
-    }
-
-    &::-webkit-scrollbar:horizontal {
-        height: 12px;
-    }
-
-    table {
-        font-size: 0.9em;
-        border-bottom: solid 1px $color-gray3;
-        border-collapse: collapse;
-
-        @include bp-custom-min(450) {
-            font-size: 1em;
-        }
-
-        .loader {
-            min-height: auto;
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            color: $color-text-secondary-dark;
-            margin: $size-4 auto 0;
-
-            span {
-                font-size: 0.9em;
-                text-shadow: 1px 1px 2px #00000033;
-            }
-        }
-
-        :deep(button) {
-            font-size: 1.2em;
-
-            &.setting {
-                font-size: 0.8em;
-                color: $color-text-secondary-dark;
-                cursor: auto;
-
-                span {
-                    white-space: wrap;
-                }
-
-                &:hover {
-                    transform: scale(1);
-                }
-            }
-        }
-
-        th,
-        td {
-            padding: $size-1;
-            text-align: center;
-        }
-
-        td {
-            font-size: 0.85em;
-            color: $color-text-secondary-dark;
-            font-weight: 500;
-            border: solid 1px $color-gray3;
-
-            &.date {
-                font-family: $secondary-font-stack;
-                font-size: 0.75em;
-                font-weight: 400;
-                color: $color-gray6;
-                padding: 0 $size-2;
-
-                span {
-                    display: flex;
-                    justify-content: center;
-                    width: 8em;
-                }
-            }
-
-            &.sorted {
-                background: darken-color($color-gray1, 2%);
-            }
-        }
-    }
-}
-
-.table-nav {
-    position: relative;
-    z-index: 2;
-    @include flexCenterAll;
-    padding-top: $size-1;
-
-    :deep(button) {
-        font-size: 1em;
-    }
-
-    span {
-        color: $color-text-secondary-dark;
-        border-bottom: solid 1px $color-gray3;
-        width: 1ch;
-        text-align: center;
     }
 }
 </style>
