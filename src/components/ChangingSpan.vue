5a11c52f0ef7d3d97736e5dd1294b870aa954ad1
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-12-04 14:16:41 -0500

    feat: added animation to only animate the changed part of the string, kept option to only have 'simple' fade-in/out animation for the entire string

--- src/components/ChangingSpan.vue
+++ src/components/ChangingSpan.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref, onMounted, nextTick, watch } from 'vue';
+import { ref, onMounted, watch, nextTick } from 'vue';
 import { useUtilAnimations } from '@/composables/animations/useUtilAnimations.js';
 
 const props = defineProps({
     text: { type: [String, Number, null], required: true },
-    enterOnly: { type: Boolean, default: false },
+    simple: { type: Boolean, default: false },
 });
 
 const { fadeIn, fadeOut } = useUtilAnimations();
@@ -12,14 +12,66 @@ const { fadeIn, fadeOut } = useUtilAnimations();
 const changingSpan = ref(null);
 const localText = ref(props.text);
 
+const items = ref([]);
+let uniqueIdCounter = 0;
+
+function createItems(str) {
+    const string = `${str}`;
+    return string.split('').map((char) => ({
+        id: uniqueIdCounter++,
+        char,
+    }));
+}
+
+function updateText(newText) {
+    const oldItems = items.value;
+    const oldText = oldItems.map((i) => i.char).join('');
+
+    let prefixLen = 0;
+    while (prefixLen < oldText.length && prefixLen < newText.length && oldText[prefixLen] === newText[prefixLen]) {
+        prefixLen++;
+    }
+
+    let oldEnd = oldText.length - 1;
+    let newEnd = newText.length - 1;
+
+    while (oldEnd >= prefixLen && newEnd >= prefixLen && oldText[oldEnd] === newText[newEnd]) {
+        oldEnd--;
+        newEnd--;
+    }
+
+    const nextItems = [];
+
+    for (let i = 0; i < prefixLen; i++) {
+        nextItems.push(oldItems[i]);
+    }
+
+    for (let i = prefixLen; i <= newEnd; i++) {
+        nextItems.push({
+            id: uniqueIdCounter++,
+            char: newText[i],
+        });
+    }
+
+    for (let i = oldEnd + 1; i < oldItems.length; i++) {
+        nextItems.push(oldItems[i]);
+    }
+
+    items.value = nextItems;
+}
+
 onMounted(() => {
-    fadeIn({ selector: changingSpan.value });
+    if (!props.simple) {
+        items.value = createItems(props.text);
+    }
 });
 
 watch(
     () => props.text,
     (newVal) => {
-        if (!props.enterOnly) {
+        if (!props.simple) {
+            updateText(newVal);
+        } else {
             fadeOut({
                 selector: changingSpan.value,
                 onComplete: async () => {
@@ -28,15 +80,49 @@ watch(
                     fadeIn({ selector: changingSpan.value });
                 },
             });
-        } else {
-            localText.value = newVal;
         }
     },
 );
+
+const onEnter = (el, done) => {
+    fadeIn({ selector: el, onComplete: done });
+};
+
+const onLeave = (el, done) => {
+    el.style.position = 'absolute';
+    fadeOut({ selector: el, onComplete: done });
+};
 </script>
 
 <template>
-    <span ref="changingSpan">
-        {{ localText }}
-    </span>
+    <template v-if="!simple">
+        <span class="changing-span-wrapper">
+            <TransitionGroup tag="span" :css="false" @enter="onEnter" @leave="onLeave">
+                <span v-for="item in items" :key="item.id" class="char-item">
+                    {{ item.char }}
+                </span>
+            </TransitionGroup>
+        </span>
+    </template>
+    <template v-else>
+        <span ref="changingSpan">{{ localText }}</span>
+    </template>
 </template>
+
+<style lang="scss" scoped>
+.changing-span-wrapper {
+    position: relative;
+    display: inline-block;
+    white-space: pre;
+}
+
+.char-item {
+    display: inline-block;
+    position: relative;
+    transition: all 0.3s ease;
+}
+
+.v-move {
+    transition: transform 0.3s ease;
+}
+</style>
